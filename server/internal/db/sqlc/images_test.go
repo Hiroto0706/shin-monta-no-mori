3fb2456eb697cb901ff0e3c78e3fc8a00f6dba9b
Hiroto0706 <mirunina2366@icloud.com>
2024-04-15 08:25:03 +0900

    add: images test codes

--- server/internal/db/sqlc/images_test.go
+++ server/internal/db/sqlc/images_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"log"
 	"shin-monta-no-mori/server/pkg/util"
 	"testing"
 
@@ -22,6 +23,20 @@ func setup(t *testing.T, db DBTX) {
 		VALUES
 		(20001, 'test_image_title_20001', 'test_image_original_src_20001', 'test_image_simple_src_20001'),
 		(20002, 'test_image_title_20002', 'test_image_original_src_20002', '');
+		`),
+		fmt.Sprintln(`
+		INSERT INTO images (id, title, original_src, simple_src)
+		VALUES
+		(30001, 'test_image_title_30001', 'test_image_original_src_30001', 'test_image_simple_src_30001'),
+		(30002, 'test_image_title_30002', 'test_image_original_src_30002', 'test_image_simple_src_30002'),
+		(30003, 'test_image_title_30003', 'test_image_original_src_30003', 'test_image_simple_src_30003');
+		`),
+		fmt.Sprintln(`
+		INSERT INTO images (id, title, original_src, simple_src)
+		VALUES
+		(40001, 'test_image_title_40001', 'test_image_original_src_40001', 'test_image_simple_src_40001'),
+		(40002, 'test_image_title_40002', 'test_image_original_src_40002', 'test_image_simple_src_40002'),
+		(40003, 'test_image_title_40003', 'test_image_original_src_40003', 'test_image_simple_src_40003');
 		`),
 	}
 	for _, query := range queries {
@@ -208,6 +223,167 @@ func TestGetImage(t *testing.T) {
 				require.Equal(t, tt.want.Title, image.Title)
 				require.Equal(t, tt.want.OriginalSrc, image.OriginalSrc)
 				require.Equal(t, tt.want.SimpleSrc.String, image.SimpleSrc.String)
+				require.NotZero(t, image.CreatedAt)
+			}
+		})
+	}
+}
+
+func TestListImage(t *testing.T) {
+	setup(t, testQueries.db)
+	defer tearDown(t, testQueries.db)
+
+	type wants struct {
+		ID          int64
+		Title       string
+		OriginalSrc string
+		SimpleSrc   sql.NullString
+	}
+	tests := []struct {
+		name    string
+		arg     ListImageParams
+		want    []wants
+		wantErr bool
+	}{
+		{
+			name: "正常系",
+			arg: ListImageParams{
+				Limit:  3,
+				Offset: 0,
+			},
+			want: []wants{
+				{
+					ID:          30003,
+					Title:       "test_image_title_30003",
+					OriginalSrc: "test_image_original_src_30003",
+					SimpleSrc: sql.NullString{
+						String: "test_image_simple_src_30003",
+						Valid:  true,
+					},
+				},
+				{
+					ID:          30002,
+					Title:       "test_image_title_30002",
+					OriginalSrc: "test_image_original_src_30002",
+					SimpleSrc: sql.NullString{
+						String: "test_image_simple_src_30002",
+						Valid:  true,
+					},
+				},
+				{
+					ID:          30001,
+					Title:       "test_image_title_30001",
+					OriginalSrc: "test_image_original_src_30001",
+					SimpleSrc: sql.NullString{
+						String: "test_image_simple_src_30001",
+						Valid:  true,
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "正常系（returnが空の時）",
+			arg: ListImageParams{
+				Limit:  3,
+				Offset: 1000,
+			},
+			want:    []wants{{}},
+			wantErr: false,
+		},
+		{
+			name: "異常系（argsの値が不正な場合）",
+			arg: ListImageParams{
+				Limit:  -1,
+				Offset: 0,
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			images, err := testQueries.ListImage(context.Background(), tt.arg)
+			log.Println(images)
+			if tt.wantErr {
+				require.Error(t, err)
+			} else {
+				for i, image := range images {
+					log.Println(i, image)
+					require.NoError(t, err)
+					require.Equal(t, tt.want[i].ID, image.ID)
+					require.Equal(t, tt.want[i].Title, image.Title)
+					require.Equal(t, tt.want[i].OriginalSrc, image.OriginalSrc)
+					require.Equal(t, tt.want[i].SimpleSrc.String, image.SimpleSrc.String)
+					require.NotZero(t, image.CreatedAt)
+				}
+			}
+		})
+	}
+}
+
+func TestUpdateImage(t *testing.T) {
+	setup(t, testQueries.db)
+	defer tearDown(t, testQueries.db)
+
+	tests := []struct {
+		name    string
+		arg     UpdateImageParams
+		want    UpdateImageParams
+		wantErr bool
+	}{
+		{
+			name: "正常系",
+			arg: UpdateImageParams{
+				ID:          40001,
+				Title:       "test_image_title_40001_edited",
+				OriginalSrc: "test_image_original_src_40001_edited",
+				SimpleSrc: sql.NullString{
+					String: "test_image_simple_src_40001_edited",
+					Valid:  true,
+				},
+			},
+			want: UpdateImageParams{
+				ID:          40001,
+				Title:       "test_image_title_40001_edited",
+				OriginalSrc: "test_image_original_src_40001_edited",
+				SimpleSrc: sql.NullString{
+					String: "test_image_simple_src_40001_edited",
+					Valid:  true,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "異常系（存在しないIDを指定している場合）",
+			arg: UpdateImageParams{
+				ID: 99999,
+			},
+			wantErr: true,
+		},
+		{
+			name: "異常系（titleが空になる場合）",
+			arg: UpdateImageParams{
+				ID:    40002,
+				Title: "",
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			image, err := testQueries.UpdateImage(context.Background(), tt.arg)
+
+			if tt.wantErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.NotEmpty(t, image)
+				require.Equal(t, tt.arg.Title, image.Title)
+				require.Equal(t, tt.arg.OriginalSrc, image.OriginalSrc)
+				require.Equal(t, tt.arg.SimpleSrc.String, image.SimpleSrc.String)
+				require.NotZero(t, image.ID)
 				require.NotZero(t, image.CreatedAt)
 			}
 		})
