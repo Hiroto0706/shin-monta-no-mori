0a53e20372527837b3e692bc89d11f29e1bd55b0
Hiroto0706 <mirunina2366@icloud.com>
2024-05-27 09:50:12 +0900

    add: set accesstoken for list illustrations api's header

--- client/src/app/admin/illustrations/page.tsx
+++ client/src/app/admin/illustrations/page.tsx
@@ -1,11 +1,19 @@
 import axios from "axios";
 import { Illustration } from "@/types/illustration";
 import Illustrations from "@/components/admin/illustrations/illustrations";
+import { getAccessToken } from "@/utils/accessToken";
 
 const fetchIllustrations = async (): Promise<Illustration[]> => {
+  const accessToken = getAccessToken();
+
   try {
     const response = await axios.get(
-      "http://localhost:8080/api/v1/admin/illustrations/list/?p=0"
+      "http://localhost:8080/api/v1/admin/illustrations/list/?p=0",
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
     );
     return response.data;
   } catch (error) {
