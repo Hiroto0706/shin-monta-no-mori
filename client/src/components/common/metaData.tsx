84df29dbead7659ce3c91e72ab685edb12049aae
Hiroto0706 <mirunina2366@icloud.com>
2024-06-29 17:46:35 +0900

    add: setting ogp image

--- client/src/components/common/metaData.tsx
+++ client/src/components/common/metaData.tsx
@@ -4,4 +4,7 @@ export const metadata: Metadata = {
   title: "もんたの森｜ゆるーくてかわいい無料イラスト",
   description:
     "もんたの森は『もんた』が書いたイラストたちを無料で保存、コピーして使用することができる無料イラストサイトです。ゆるくてかわいいもの大好きな『もんた』が趣味で描いてる絵を暇な時にアップロードしています！",
+  openGraph: {
+    images: ["/site-image.png"],
+  },
 };
