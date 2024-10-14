d1d7bd77903e734293b8ab1935a2ed8328e7702c
Hiroto0706 <mirunina2366@icloud.com>
2024-10-14 16:08:49 +0900

    add: speedinsight component

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
 import { metadata } from "@/components/common/metaData";
 export { metadata };
@@ -15,6 +16,7 @@ export default function RootLayout({
   return (
     <>
       <Analytics />
+      <SpeedInsights />
       <html lang="jp" className="text-gray-600">
         <body className={inter.className}>
           <div>{children}</div>
