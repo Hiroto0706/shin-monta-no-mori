c2b204d19b83efc24df9c108016877edbe74c432
Hiroto0706 <mirunina2366@icloud.com>
2025-01-12 22:24:04 +0900

    fix: next config

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   images: {
     domains: ['storage.googleapis.com'],
   },
-  staticPageGenerationTimeout: 960,
+  staticPageGenerationTimeout: 480,
 };
 
 export default nextConfig;
